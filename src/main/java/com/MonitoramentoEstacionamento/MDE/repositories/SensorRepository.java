7cdd01eeaa7bc2b5a227da2444cea1c3cb3f5cf5
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-11-08 16:44:46 -0300

    Feat: repositorios completas

--- src/main/java/com/MonitoramentoEstacionamento/MDE/repositories/SensorRepository.java
+++ src/main/java/com/MonitoramentoEstacionamento/MDE/repositories/SensorRepository.java
@@ -1,10 +1,10 @@
 package com.MonitoramentoEstacionamento.MDE.repositories;
 
-import com.MonitoramentoEstacionamento.MDE.entities.Estacionamento;
+import com.MonitoramentoEstacionamento.MDE.entities.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EstacionamentoRepository extends JpaRepository<Estacionamento, Integer> {
+public interface SensorRepository extends JpaRepository<Sensor, Integer> {
     // Adicione métodos personalizados, se necessário
 }
